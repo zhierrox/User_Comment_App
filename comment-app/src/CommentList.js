1a1e19e1df881dcadf599d5e5319c6dde5b8fbf7
Jiao <jiao@Jiaos-MacBook-Pro.local>
2018-08-10 14:56:58 -0700

    implement commentInput to commentApp callback

---
+++ comment-app/src/CommentList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Comment from './Comment';
+
+class CommentList extends Component {
+    render() {
+        return (
+            <div>
+                CommentList
+                <Comment />
+            </div>
+        );
+    }
+}
+
+export default CommentList;
